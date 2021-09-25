5e8f95ff218c36e56914cf870b5aeb8a5eb3e4c3
deepakasara <84222618+deepakasara@users.noreply.github.com>
2021-09-26 00:48:59 +0530

    system changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,11 +9,11 @@ module.exports = {
       production: {
         user: "asaratech",
         host: "184.168.123.205",
-        path: "/var/test/nodejs_sample_app",
+        path: "/var/test/nodejs_sample_app/",
         repo: "git@github.com:deepuRai/nodejs_sample_app.git",
         ref: "origin/main",
         
-        "post-deploy": "npm i; pm2 reload ecosystem.config.js --env production",
-      },
-    },
+        "post-deploy": "npm i; pm2 reload ecosystem.config.js --env production"
+      }
+    }
   };
